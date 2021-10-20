d6b610812694d76928056d4baa7c53470ce357be
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-10-20 09:53:15 -0300

    Use cookies to keep a email addres active

--- temp_mail.py
+++ temp_mail.py
@@ -3,24 +3,36 @@ import requests
 from bs4 import BeautifulSoup
 
 class TempMail:
-    endpoint = 'https://10minutemail.org'
-    new = '/new.html'
-    renew = '/more.html'
+    __endpoint = 'https://10minutemail.org'
+    __endpoint_new = 'https://10minutemail.org/new.html'
+    __endpoint_renew = 'https://10minutemail.org/more.html'
+    __endpoint_hundred_minutes = 'https://10minutemail.org/more100.html'
 
-    def __init__(self):
+    def __init__(self, session_id=None):
         self.email = ''
         self.messages = []
+        self.cookies = {}
 
-    def get_new_email(self):
-        ui = self.__get_site_parsed(self.endpoint)
+        if session_id != None:
+            self.cookies['PHPSESSID'] = session_id
 
-        self.email = ui.find('input', 'mailtext', value=True)['value']
+        self.__set_email()
+
+    def get_session_id(self) -> str:
+        return self.cookies.get('PHPSESSID', None)
+
+    def change_email_address(self) -> None:
+        tmm = self.__get_site_parsed(self.__endpoint_new)
+
+        self.email = tmm.find('input', 'mailtext', value=True)['value']
     
-    def check_messages(self):
-        ui = self.__get_site_parsed(self.endpoint)
+    def check_messages(self) -> None:
+        tmm = self.__get_site_parsed(self.__endpoint)
 
-        table = ui.find('table', id='maillist')
+        table = tmm.find('table', id='maillist')
         table_lines = table.find_all('tr', onclick=True)
+
+        self.messages = []
 
         for line in table_lines:
             columns = line.find_all('td')
@@ -39,7 +51,16 @@ class TempMail:
 
             self.messages.append(mail)
 
-    def __get_site_parsed(self, endpoint: str):
-        response = requests.get(endpoint)
-        content = response.content
-        return BeautifulSoup(content, 'html.parser')
+    def __get_site_parsed(self, endpoint: str) -> BeautifulSoup:
+        response = requests.get(endpoint, cookies=self.cookies)
+        session_id = response.cookies.get_dict().get('PHPSESSID', None)
+        
+        if session_id != None:
+            self.cookies['PHPSESSID'] = session_id
+
+        return BeautifulSoup(response.content, 'html.parser')
+    
+    def __set_email(self) -> None:
+        tmm = self.__get_site_parsed(self.__endpoint)
+
+        self.email = tmm.find('input', 'mailtext', value=True)['value']
