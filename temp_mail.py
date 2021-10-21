39211ced4b8bd6f7f1a3f97bb2f1717049996fee
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-10-21 14:07:42 -0300

    Add read mail e get more minutes methods

--- temp_mail.py
+++ temp_mail.py
@@ -4,9 +4,10 @@ from bs4 import BeautifulSoup
 
 class TempMail:
     __endpoint = 'https://10minutemail.org'
+    __endpoint_message = 'https://10minutemail.org/readmail.html?mid='
     __endpoint_new = 'https://10minutemail.org/new.html'
-    __endpoint_renew = 'https://10minutemail.org/more.html'
-    __endpoint_hundred_minutes = 'https://10minutemail.org/more100.html'
+    __endpoint_more_10 = 'https://10minutemail.org/more.html'
+    __endpoint_more_100 = 'https://10minutemail.org/more100.html'
 
     def __init__(self, session_id=None):
         self.email = ''
@@ -17,6 +18,7 @@ class TempMail:
             self.cookies['PHPSESSID'] = session_id
 
         self.__set_email()
+        self.check_messages()
 
     def get_session_id(self) -> str:
         return self.cookies.get('PHPSESSID', None)
@@ -25,6 +27,12 @@ class TempMail:
         tmm = self.__get_site_parsed(self.__endpoint_new)
 
         self.email = tmm.find('input', 'mailtext', value=True)['value']
+
+    def get_10_more_minutes(self) -> None:
+        tmm = self.__get_site_parsed(self.__endpoint_more_10)
+    
+    def get_100_more_minutes(self) -> None:
+        tmm = self.__get_site_parsed(self.__endpoint_more_100)
     
     def check_messages(self) -> None:
         tmm = self.__get_site_parsed(self.__endpoint)
@@ -41,16 +49,33 @@ class TempMail:
             location = line['onclick']
             location = location.replace('location=', '')
             location = location.replace("'", "")
-            
+            id = location.replace('readmail.html?mid=', '')
+
+            if id == 'welcome': continue
+
+            sender = columns[0]
+            sender_address = self.__decrypt_cf_email(sender)
+            sender_name = sender.text.split('<')[0]
+
             mail = {
-                'sender': columns[0].text,
+                'id': id,
+                'sender_name': sender_name,
+                'sender_address': sender_address,
                 'subject': columns[1].text,
                 'timeAgo': columns[2].text,
-                'endpoint': location
             }
 
             self.messages.append(mail)
 
+    def read_mail(self, id: str):
+        endpoint = self.__endpoint_message + id
+        tmm = self.__get_site_parsed(endpoint)
+
+        mail_html = tmm.find('div', id='tab3')
+        mail = mail_html.find('p', 'mailinhtml')
+
+        return self.__sanitize_message(mail)
+    
     def __get_site_parsed(self, endpoint: str) -> BeautifulSoup:
         response = requests.get(endpoint, cookies=self.cookies)
         session_id = response.cookies.get_dict().get('PHPSESSID', None)
@@ -64,3 +89,26 @@ class TempMail:
         tmm = self.__get_site_parsed(self.__endpoint)
 
         self.email = tmm.find('input', 'mailtext', value=True)['value']
+
+    def __decrypt_cf_email(self, soup: BeautifulSoup):
+        mail_tag = soup.find(['a', 'span'], '__cf_email__')
+        cfemail = mail_tag['data-cfemail']
+        return self.__decode(cfemail)
+    
+    def __decode(self, cfemail):
+        enc = bytes.fromhex(cfemail)
+        return bytes([c ^ enc[0] for c in enc[1:]]).decode('utf8')
+
+    def __sanitize_message(self, mail: BeautifulSoup) -> str:
+        for br in mail.find_all("br"):
+            br.replace_with("\n")
+        
+        sanitized_mail = ''
+
+        for mail_content in mail.contents:
+            if mail_content.text.strip() == '[email protected]':
+                sanitized_mail += self.__decrypt_cf_email(mail_content)
+            else:
+                sanitized_mail += mail_content
+
+        return sanitized_mail
--- test.py
+++ test.py
@@ -1,29 +1,53 @@
+import os
 from temp_mail import TempMail
 
-# tm = TempMail(session_id='2e54fbff3e78b522296ed36e9dbfd01c')
-tm = TempMail()
-print(f'email: {tm.email}')
+clear = lambda: os.system('cls')
+
+tm = TempMail(session_id='127f5be450d212f6b7a9d293593e8ab4')
+# tm = TempMail()
 
 while True:
     try:
         print()
+        print(f'email: {tm.email}')
         print('1 - get new mail')
         print('2 - list messages')
-        print('3 - show email')
-        print('4 - show cookie')
+        print('3 - read message')
+        print('4 - show session id')
+        print('8 - get 10 more minutes')
+        print('9 - get 100 more minutes')
         print('0 - close')
         user_input = int(input('select your option: ').strip())
 
+        clear()
+
+        # get new mail
         if user_input == 1:
             tm.change_email_address()
             print(f'novo email: {tm.email}')
+
+        # list messages
         elif user_input == 2:
             tm.check_messages()
             print(tm.messages)
+
+        # read message
         elif user_input == 3:
-            print(tm.email)
+            mail_id = input('message id: ').strip()
+            print(tm.read_mail(mail_id))
+
+        # show session id
         elif user_input == 4:
             print(tm.get_session_id())
+
+        # get 10 more minutes
+        elif user_input == 8:
+            tm.get_10_more_minutes()
+
+        # get 100 more minutes
+        elif user_input == 9:
+            tm.get_100_more_minutes()
+
         elif(user_input == 0):
             break
     except ValueError:
